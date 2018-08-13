b2b1fec362db21e4ea40b3a1d0273048b53b956c
lihongle <lhl_1986@qq.com>
2018-08-13 17:02:28 +0800

    feat:04.01.Ribbon+Hystrix

---
+++ chapter04/section01/src/main/java/cn/ep/spring/boot/ch04/s01/ServiceRibbonHystrixApplication.java
@@ -0,0 +1,28 @@
+package cn.ep.spring.boot.ch04.s01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class ServiceRibbonHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonHystrixApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
